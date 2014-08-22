f8117c37723ae86dbfcf6933598fd575d5a44517
bhellion <spambin_10@hotmail.com>
2014-08-21 22:15:28 -0700

    new file: ex15

---
+++ ex15.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+file_again = raw_input("Type a filename to read ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
