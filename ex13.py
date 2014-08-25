759cabbf51194a083d306ef92dd7a14d5b09e2f0
bhellion <spambin_10@hotmail.com>
2014-08-24 21:59:04 -0700

    new file: ex13

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+fourth = raw_input('Put something else here ')
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Yada yada " + fourth
